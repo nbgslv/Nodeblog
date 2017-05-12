c41a5f69aaabeeb44716f44965ead300f79a4782
nbgslv <nbgslv@gmail.com>
2017-05-12 14:22:58 +0300

    Show single post and Add comments per post

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,18 @@ var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
 /* GET users listing. */
+router.get('/show/:id', function(req, res, next) {
+  var posts = db.get('posts');
+
+  posts.findById(req.params.id, function(err, posts){
+    res.render('show', {
+      'post': posts
+    });
+  });
+});
+
+
+/* GET users listing. */
 router.get('/add', function(req, res, next) {
   var categories = db.get('categories');
 
@@ -15,7 +27,6 @@ router.get('/add', function(req, res, next) {
   		'title': 'Add Post',
   		'categories': categories
   	});
-  	console.log(categories);
   });
 });
 
@@ -63,6 +74,58 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
   			}
   		});
   	}
+});
+
+router.post('/addcomment', function(req, res, next) {
+  // Get form values
+  var name = req.body.name;
+  var email = req.body.email;
+  var body = req.body.body;
+  var postid = req.body.postid;
+  var commentdate = new Date();
+
+  // Form validation
+  req.checkBody('name', 'Name field is required.').notEmpty();
+  req.checkBody('email', 'Email field is required.').notEmpty();
+  req.checkBody('email', 'Email is not a valid Email address.').isEmail();
+  req.checkBody('body', 'Body field is required.').notEmpty();
+    // Check form errors
+    var errors = req.validationErrors();
+
+    if(errors){
+      var posts = db.get('posts');
+      posts.findById(postid, function(err, post){
+        res.render('show', {
+          'errors': errors,
+          'posts' : post
+        });
+      });
+    } else {
+      var comment = {
+        "name": name,
+        "email": email,
+        "body": body,
+        "commentdate": commentdate
+      }
+
+      var posts = db.get('posts');
+
+      posts.update({
+        "_id": postid
+      },{
+          $push:{
+              "comments": comment
+          }
+      }, function(err, doc){
+        if(err){
+          throw err;
+        } else {
+          req.flash('success', 'Comment Added');
+          res.location('/posts/show/'+postid);
+          res.redirect('/posts/show/'+postid);
+        }
+      });
+    }
 });
 
 module.exports = router;
